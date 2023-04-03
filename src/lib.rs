c34319fc9215b4ee4cde8ba407fc339f51079e8a
Kidan Nelson <nelsonkidan@gmail.com>
2023-04-02 23:36:38 -0400

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-// pub mod neuron;
-// pub mod synapse;
-// pub mod network;
-// pub mod learning;
-// pub mod simulation;
-// pub mod utility;
-// pub mod io;
+pub mod neuron;
+pub mod synapse;
+pub mod network;
+pub mod learning;
+pub mod simulation;
+pub mod utility;
+pub mod io;
